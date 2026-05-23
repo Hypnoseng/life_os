cf1e0eafaca72be6ee93cae1bd9bbe2b4ab22101
Hypnoseng <hypnos.eng.dev@gmail.com>
2026-05-23 10:53:03 +0700

    fix deploy

--- core/apps.py
+++ core/apps.py
@@ -1,5 +1,13 @@
 from django.apps import AppConfig
-
+from django.contrib.auth.models import User
 
 class CoreConfig(AppConfig):
     name = 'core'
+
+def create_superuser():
+    if not User.objects.filter(username="admin").exists():
+        User.objects.create_superuser(
+            "admin",
+            "admin@example.com",
+            "password123"
+        )
---
+++ core/your_app/management/commands/createsu.py
@@ -0,0 +1,18 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth.models import User
+import os
+
+class Command(BaseCommand):
+    def handle(self, *args, **kwargs):
+        username = os.environ.get("DJANGO_SUPERUSER_USERNAME")
+        password = os.environ.get("DJANGO_SUPERUSER_PASSWORD")
+        email = os.environ.get("DJANGO_SUPERUSER_EMAIL", "")
+
+        if not username or not password:
+            return
+
+        if not User.objects.filter(username=username).exists():
+            User.objects.create_superuser(username, email, password)
+            print("Superuser created")
+        else:
+            print("Superuser already exists")
